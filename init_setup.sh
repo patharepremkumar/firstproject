be5c3e66aa4f37857a79a2ca00ef7e4fbc2ddce4
patharepremkumar <patharepremkumar@gmail.com>
2024-01-13 17:21:31 +0530

    Exception model added

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,9 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.10 version" 
-"C:/Program Files/Python310/python.exe" -m venv myenv
+"C:/Program Files/Python310/python.exe" -m venv myvenv
 echo [$(date)]: "activating the environment" 
-"F:\MLOPS\firstproject"  myenv\Scripts\Activate.ps1 
+"F:/MLOPS/firstproject/" activate myvenv
 echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
+
